a92a904e9320f55c4f315a66dc5371a4f411dda4
Kris <kristaps97@gmail.com>
2020-11-26 17:20:41 +0000

    Added models for an article, comment and a user

---
+++ TopHeadlinesApp/TopHeadlinesApp.UI/Models/Article.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopHeadlinesApp.UI.Models
+{
+   public class Article
+   {
+      public int Id { get; set; }
+      public string Title { get; set; }
+      public string Author { get; set; }
+      public DateTime PublishedAt { get; set; }
+      public ObservableCollection<Comment> Comments { get; set; }
+   }
+}
