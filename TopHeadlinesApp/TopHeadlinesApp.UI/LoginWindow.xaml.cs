f5e853e2c140c8dfc95482b55817483ff3702d1e
Kris <kristaps97@gmail.com>
2020-11-26 17:11:27 +0000

    Added simple login window that creates a new Main Window with the appropriate username

---
+++ TopHeadlinesApp/TopHeadlinesApp.UI/LoginWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TopHeadlinesApp.UI
+{
+   /// <summary>
+   /// Interaction logic for LoginWindow.xaml
+   /// </summary>
+   public partial class LoginWindow : Window
+   {
+      public LoginWindow()
+      {
+         InitializeComponent();
+      }
+
+      private void loginButton_Click(object sender, RoutedEventArgs e)
+      {
+         // TODO : Implement proper login logic
+
+         string username = usernameTextBox.Text;
+         string password = passwordTextBox.Password.ToString();
+
+         if(!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+         {
+            MainWindow mainWindow = new MainWindow(username);
+            mainWindow.Show();
+            Close();
+         }         
+      }
+   }
+}
--- TopHeadlinesApp/TopHeadlinesApp.UI/MainWindow.xaml.cs
+++ TopHeadlinesApp/TopHeadlinesApp.UI/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace TopHeadlinesApp.UI
    /// </summary>
    public partial class MainWindow : Window
    {
-      public MainWindow()
+      public MainWindow(string username)
       {
          InitializeComponent();
       }
