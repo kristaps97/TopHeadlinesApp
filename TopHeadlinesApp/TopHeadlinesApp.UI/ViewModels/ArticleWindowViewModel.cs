dd2e79e8b8f9ea4691c9bbb7ff348508364a9738
Kris <kristaps97@gmail.com>
2020-11-26 18:08:40 +0000

    Added a view model for the Article Window Created method to add new comments to the list of comments

---
+++ TopHeadlinesApp/TopHeadlinesApp.UI/ViewModels/ArticleWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopHeadlinesApp.UI.Models;
+
+namespace TopHeadlinesApp.UI.ViewModels
+{
+   public class ArticleWindowViewModel
+   {
+      public Article Article { get; set; }
+      public User User { get; set; }
+      public string CurrentComment { get; set; }
+
+      public ArticleWindowViewModel(Article article, User user)
+      {
+         Article = article;
+         User = user;
+      }
+
+      public void AddComment()
+      {
+         Article.Comments.Add(new Comment
+         {
+            UserId = User.Username,
+            Content = CurrentComment
+         });
+      }
+   }
+}
