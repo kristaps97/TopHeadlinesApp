6de9ccb174bc421c7f766b8810b0bf9e3d8e3a04
Kris <kristaps97@gmail.com>
2020-11-26 18:48:13 +0000

    Created local database context and ran initial migration

---
+++ TopHeadlinesApp/TopHeadlinesApp.UI/DataAccess/ArticleContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopHeadlinesApp.UI.Models;
+
+namespace TopHeadlinesApp.UI.DataAccess
+{
+   public class ArticleContext : DbContext
+   {
+      public ArticleContext() : base("ArticlesDbContext")
+      {
+
+      }
+
+      public DbSet<User> Users { get; set; }
+      public DbSet<Article> Articles { get; set; }
+      public DbSet<Comment> Comments { get; set; }
+
+   }
+}
---
+++ TopHeadlinesApp/TopHeadlinesApp.UI/Migrations/202011261842374_Initial_database_setup.cs
@@ -0,0 +1,55 @@
+﻿namespace TopHeadlinesApp.UI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial_database_setup : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Articles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Author = c.String(),
+                        PublishedAt = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Comments",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(),
+                        Content = c.String(),
+                        Article_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Articles", t => t.Article_Id)
+                .Index(t => t.Article_Id);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Username = c.String(),
+                        Password = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Comments", "Article_Id", "dbo.Articles");
+            DropIndex("dbo.Comments", new[] { "Article_Id" });
+            DropTable("dbo.Users");
+            DropTable("dbo.Comments");
+            DropTable("dbo.Articles");
+        }
+    }
+}
